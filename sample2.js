3584893b770ab614956ee5ea014bd5643e0538c0
Deepak Kumar <deepakk.ug20.ee@nitp.ac.in>
2023-11-30 14:51:45 +0530

    solution completed

--- sample2.js
+++ sample2.js
@@ -4,6 +4,28 @@ import axios from "axios";
 
 const app=express();
 const port=3000;
-
+//  built  in middle ware which allow the satatic files of the front-end
 app.use(express.static("public"));
-app.use(bodyParse.)
+// when the user goes to the the home page it should render the index.ejs file.
+app.get("/",async (req,res)=>
+{
+    // we have to return something here that what we are doing here...
+    try
+    {
+        const result=await axios.get("https://secrets-api.appbrewery.com/random");
+    res.render("index.ejs",{
+        secret:result.data.secret,
+        user:resilt.data.username
+    });
+    }
+    catch(error)
+    {
+        console.log(error.response.data);
+        res.status(500);
+    }
+});
+
+
+app.listen(port ,()=>{
+    console.log(`Server started onport ${port}`);
+});
